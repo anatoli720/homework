84beea05cc14aa8734ec45b9ba7ac4b2e6bcaa13
anatoli720 <anatoli7204n@gmail.com>
2024-12-10 20:27:02 +0300

    first commit

---
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -0,0 +1,26 @@
+import {UserType} from '../HW8'
+
+type ActionType =
+    | { type: 'sort'; payload: 'up' | 'down' }
+    | { type: 'check'; payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+    switch (action.type) {
+        case 'sort': { // by name
+
+            return [...state].sort((a,b)=>{
+if(action.payload === 'up'){
+    return a.name.localeCompare(b.name)
+} else {
+    return b.name.localeCompare(a.name)
+}
+            }) // need to fix
+        }
+        case 'check': {
+
+            return state.filter(u => u.age >= 18)// need to fix
+        }
+        default:
+            return state
+    }
+}
